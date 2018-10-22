389332e5aa6d16da005edd6d9c93eb555cad11ba
daniMP <danimontes@hotmail.es>
2018-10-22 20:56:35 +0200

    Commit factorial 1

---
+++ src/main/java/com/soa/ws/RestWs/Fact.java
@@ -0,0 +1,20 @@
+package com.soa.ws.RestWs;
+
+public class Fact {
+
+    public int num;
+
+    public Fact( int numero) {
+
+        this.num = numero;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
+
